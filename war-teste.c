cb8fde104b320ea8b4224d670b049e1cb847ca2a
brunoricardods-lab <brunoricardods@gmail.com>
2025-11-29 21:59:50 +0000

    teste de codigo fonte

--- war-teste.c
+++ war-teste.c
@@ -1,4 +1,29 @@
-#include <stdio.h>
-int main(){
-    printf("Desafio War! \n");
+#include <stdio.h> 
+#define NUM_PAISES 5
+#define TAMANHO_NOME 30
+
+int main() {
+    
+    char paises[NUM_PAISES][TAMANHO_NOME] = {
+        "Brasil",
+        "Canada",
+        "Japao",
+        "Alemanha",
+        "Australia"
+    };
+
+    int i; 
+
+    printf("==================================\n");
+    printf(" Lista de %d Países Cadastrados \n", NUM_PAISES);
+    printf("==================================\n");
+    
+    for (i = 0; i < NUM_PAISES; i++) {
+        
+        printf("%d. %s\n", i + 1, paises[i]);
+    }
+
+    printf("==================================\n");
+    
+    return 0; 
 }
